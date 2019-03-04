82e766ac50836171222c752ad40672a4ce7db0d8
vladtrebukhov <vlad.trebukhov123@gmail.com>
2019-03-03 21:14:48 -0500

    added PUT request/method override to edit goals

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const port = 5000;
 
 const app = express();
@@ -10,37 +11,72 @@ const app = express();
 require('./models/Goal');
 const Goal = mongoose.model('goals');
 
+mongoose
+  .connect('mongodb://vladtrebukhov:vlad123@ds019638.mlab.com:19638/goalsapp', {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Connected to database'))
+  .catch(error => console.log(error));
+
 // middleware
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main'
+  })
+);
 app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+// Middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 app.use(bodyParser.json());
+app.use(methodOverride('_method'))
 
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`)
-})
+  console.log(`Listening on port: ${port}`);
+});
 
 app.get('/', (req, res) => {
   const title = '#Goals';
   res.render('homepage', {
     title: title
   });
-})
+});
 
 app.get('/about', (req, res) => {
   res.render('about');
-})
+});
 
 app.get('/goals/add', (req, res) => {
   res.render('addgoals');
-})
+});
 
+app.get('/goals/edit/:id', (req, res) => {
+  Goal.findOne({
+    _id: req.params.id
+  }).then(goal =>
+    res.render('edit', {
+      goal: goal
+    })
+  );
+});
+
+app.get('/goals', (req, res) => {
+  Goal.find({})
+    .sort({
+      date: 'desc'
+    })
+    .then(goals => {
+      res.render('goals', {
+        goals: goals
+      });
+    });
+});
 
 // bodyparser middleware is required to catch the values from the form
 app.post('/goals', (req, res) => {
@@ -49,13 +85,13 @@ app.post('/goals', (req, res) => {
   if (!req.body.name) {
     errors.push({
       text: 'Please enter a name for your goal.'
-    })
+    });
   }
 
   if (!req.body.details) {
     errors.push({
       text: 'Please enter some details for your goal.'
-    })
+    });
   }
 
   if (errors.length > 0) {
@@ -63,8 +99,34 @@ app.post('/goals', (req, res) => {
       errors: errors,
       name: req.body.name,
       details: req.body.details
-    })
+    });
   } else {
-    res.send('ok')
+    // save to database
+    const newUser = {
+      name: req.body.name,
+      details: req.body.details
+    };
+
+    new Goal(newUser).save().then(goal => {
+      res.redirect('/goals');
+    });
   }
+});
+
+// Edit form processing
+//* * must use the module method-override here because we can't just change
+// the POST request to a PUT request inside the form of edit.handlebars */
+app.put('/goals/:id', (req, res) => {
+  Goal.findOne({
+    _id: req.params.id
+  }).then(goal => {
+    // new values
+    goal.name = req.body.name;
+    goal.details = req.body.details;
+
+    goal.save()
+      .then(goal => {
+        res.redirect('/goals');
+      });
+  })
 })
